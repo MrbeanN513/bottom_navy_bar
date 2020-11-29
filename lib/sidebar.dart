c7c436410fd664adc0ac9e425ce195910b26a967
MrbeanN513 <56640484+MrbeanN513@users.noreply.github.com>
2020-11-29 22:49:19 +0530

    Update sidebar.dart

--- lib/sidebar.dart
+++ lib/sidebar.dart
@@ -18,7 +18,7 @@ class SideBarr extends StatelessWidget {
     @required this.onItemSelected,
     this.curve = Curves.linear,
   })  : assert(items != null),
-        assert(items.length >= 2 && items.length <= 5),
+        assert(items.length >= 2 && items.length <= 8),
         assert(onItemSelected != null),
         assert(animationDuration != null),
         assert(curve != null),
