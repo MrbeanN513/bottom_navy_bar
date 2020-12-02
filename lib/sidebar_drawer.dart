6a39be25262559730aa83d7e94a9281f995180d8
MrbeanN513 <haldergoutam9@gmail.com>
2020-12-02 17:02:28 +0530

    gdd

---
+++ lib/sidebar_drawer.dart
@@ -0,0 +1,218 @@
+import 'package:bottom_navy_bar/button_tv.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter/widgets.dart';
+
+class SideBarrDrawer extends StatelessWidget {
+  SideBarrDrawer({
+    Key key,
+    this.height = 500,
+    this.width = 200,
+    this.selectedIndex = 0,
+    this.showElevation = true,
+    this.iconSize = 24,
+    this.backgroundColor,
+    this.itemCornerRadius = 50,
+    this.containerHeight = 56,
+    this.animationDuration = const Duration(milliseconds: 270),
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.crossAxisAlignment = CrossAxisAlignment.start,
+    @required this.items,
+    @required this.onItemSelected,
+    this.curve = Curves.linear,
+  })  : assert(items != null),
+        assert(items.length >= 2 && items.length <= 18),
+        assert(onItemSelected != null),
+        assert(animationDuration != null),
+        assert(curve != null),
+        super(key: key);
+
+  final int selectedIndex;
+
+  final double iconSize;
+
+  final Color backgroundColor;
+
+  final bool showElevation;
+
+  final Duration animationDuration;
+
+  final List<SideBarrDrawerItem> items;
+
+  final ValueChanged<int> onItemSelected;
+
+  final MainAxisAlignment mainAxisAlignment;
+  final CrossAxisAlignment crossAxisAlignment;
+
+  final double itemCornerRadius;
+
+  final double containerHeight;
+  final double height;
+  final double width;
+
+  final Curve curve;
+
+  @override
+  Widget build(BuildContext context) {
+    final bgColor = (backgroundColor == null)
+        ? Theme.of(context).bottomAppBarColor
+        : backgroundColor;
+
+    return Container(
+      decoration: BoxDecoration(
+        color: bgColor,
+        boxShadow: [
+          if (showElevation)
+            const BoxShadow(
+              color: Colors.black12,
+              blurRadius: 2,
+            ),
+        ],
+      ),
+      child: SafeArea(
+        child: Container(
+          width: width,
+          height: height,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              crossAxisAlignment: crossAxisAlignment,
+              mainAxisAlignment: mainAxisAlignment,
+              children: items.map((item) {
+                var index = items.indexOf(item);
+                return GestureDetector(
+                  onTap: () => onItemSelected(index),
+                  child: SidebarDpad(
+                    onTap: () => onItemSelected(index),
+                    child: _ItemWidget(
+                      item: item,
+                      iconSize: iconSize,
+                      isSelected: index == selectedIndex,
+                      backgroundColor: bgColor,
+                      itemCornerRadius: itemCornerRadius,
+                      animationDuration: animationDuration,
+                      curve: curve,
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ItemWidget extends StatelessWidget {
+  final double iconSize;
+  final bool isSelected;
+  final SideBarrDrawerItem item;
+  final Color backgroundColor;
+  final double itemCornerRadius;
+  final Duration animationDuration;
+  final Curve curve;
+
+  const _ItemWidget({
+    Key key,
+    @required this.item,
+    @required this.isSelected,
+    @required this.backgroundColor,
+    @required this.animationDuration,
+    @required this.itemCornerRadius,
+    @required this.iconSize,
+    this.curve = Curves.linear,
+  })  : assert(isSelected != null),
+        assert(item != null),
+        assert(backgroundColor != null),
+        assert(animationDuration != null),
+        assert(itemCornerRadius != null),
+        assert(iconSize != null),
+        assert(curve != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      container: true,
+      selected: isSelected,
+      child: AnimatedContainer(
+        width: isSelected ? double.maxFinite : double.maxFinite,
+        height: 50,
+        duration: animationDuration,
+        curve: curve,
+        decoration: BoxDecoration(
+          color:
+              isSelected ? item.activeColor.withOpacity(0.2) : backgroundColor,
+          borderRadius: BorderRadius.circular(itemCornerRadius),
+        ),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          physics: NeverScrollableScrollPhysics(),
+          child: Container(
+            width: isSelected ? 130 : 130,
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                IconTheme(
+                  data: IconThemeData(
+                    size: iconSize,
+                    color: isSelected
+                        ? item.activeColor.withOpacity(1)
+                        : item.inactiveColor == Colors.black
+                            ? item.activeColor
+                            : item.inactiveColor,
+                  ),
+                  child: item.icon,
+                ),
+                // if (isSelected)
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 4),
+                    child: DefaultTextStyle.merge(
+                      style: TextStyle(
+                        color: isSelected
+                            ? item.activeColor.withOpacity(1)
+                            : item.inactiveColor == Colors.black
+                                ? item.activeColor
+                                : item.inactiveColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      maxLines: 1,
+                      textAlign: item.textAlign,
+                      child: item.title,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SideBarrDrawerItem {
+  SideBarrDrawerItem({
+    @required this.icon,
+    @required this.title,
+    this.activeColor = Colors.blue,
+    this.textAlign,
+    this.inactiveColor,
+  })  : assert(icon != null),
+        assert(title != null);
+
+  final Widget icon;
+
+  final Widget title;
+
+  final Color activeColor;
+
+  final Color inactiveColor;
+
+  final TextAlign textAlign;
+}
